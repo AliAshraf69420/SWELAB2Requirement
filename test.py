c3eeb71cd070fe014eabe6c4b245aa683a901516
Ali <AliAwesomethe22th@gmail.com>
2024-10-10 12:21:15 +0300

    still problem, different one

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def home():
 	return render_template('index.html')
-@app.route('/submit')
+@app.route('/submit', methods=['POST'])
 def submit():
-    return render_template(submit.html)
+ name = request.form.get["name"]
+ age = request.form.get["age"]
+ return "test"
 if __name__ == '__main__':
 	app.run(debug=True)
